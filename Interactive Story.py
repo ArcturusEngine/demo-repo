8d3854b0bcc2313ca0d4ed61ce056759a7645e77
ArcturusEngine <148978260+ArcturusEngine@users.noreply.github.com>
2023-10-25 09:16:02 -0400

    Add files via upload

---
+++ Interactive Story.py
@@ -0,0 +1,12 @@
+# Interactive Story
+playerInv = []
+playerHP = 100
+
+print("You find yoursef at the edge of a forest."
+       "In front of you lies two paths that diverge to the left and right of you."
+       "The path on the left is dead silent; it's so dark, you can hardly see anything down there."
+       " ")
+
+decision1 = input("Which path will you go take? the \"Left\" or the \"Right\"? ")
+if decision1 == "Right" or decision1 == "R" or decision1 == "right" or decision1 == "r":
+    print("You decide to f")
